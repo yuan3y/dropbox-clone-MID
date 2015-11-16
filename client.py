ecafdd71e840271577370fea94211c565386aba6
GavrilinEugene <julentver1@gmail.com>
2015-11-16 15:03:53 +0100

    test print with modification time of files

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-import os
+import os, time
 from os.path import isfile, join
 '''
 [ f for f in os.listdir('./') if isfile(join('./',f)) ]
@@ -7,12 +7,14 @@ from os.path import isfile, join
 
 print(os.listdir("./"))
 filenames_list =  os.listdir("./")
+filter(os.path.isfile, os.listdir( os.curdir ))
+# filter(os.path.isfile, filenames_list)
 
 for filename in filenames_list:
-    with open('./%s' % filename, 'r') as file:
-        data = file.read()
-        requests.post({'filename': filename, 'data':data})
+    # with open('./%s' % filename, 'r') as file:
+    #     data = file.read()
+    #     requests.post({'filename': filename, 'data':data})
+    requests.post("http://127.0.0.1:5000", data={'filename': filename, 'timemodification': time.ctime(os.path.getmtime(filename))})
 
-payload = {'key1': 'value1', 'key2': 'value2'}
-requests.post("http://127.0.0.1:5000", data=payload)
-requests.post("http://127.0.0.1:5000", data=payload)
+# payload = {'key1': 'value1', 'key2': 'value2'}
+# requests.post("http://127.0.0.1:5000", data=payload)
