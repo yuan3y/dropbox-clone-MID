d2fe00f1692a2601f0f2d1025537783f77400760
Yuan Yiyang <yuan3y@gmail.com>
2015-11-16 15:06:24 +0100

    print out filesize

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 import os, time
-from os.path import isfile, join
+from os.path import isfile, join, getsize
 '''
 [ f for f in os.listdir('./') if isfile(join('./',f)) ]
 '''
@@ -14,7 +14,7 @@ for filename in filenames_list:
     # with open('./%s' % filename, 'r') as file:
     #     data = file.read()
     #     requests.post({'filename': filename, 'data':data})
-    requests.post("http://127.0.0.1:5000", data={'filename': filename, 'timemodification': time.ctime(os.path.getmtime(filename))})
+    requests.post("http://127.0.0.1:5000", data={'filename': filename, 'timemodification': time.ctime(os.path.getmtime(filename)), 'filesize': os.path.getsize(filename)})
 
 # payload = {'key1': 'value1', 'key2': 'value2'}
 # requests.post("http://127.0.0.1:5000", data=payload)
