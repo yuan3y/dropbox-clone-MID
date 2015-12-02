47de5119f3a905f8b1f2db81a3c8a45ea447ecdf
Yuan Yiyang <yuan3y@gmail.com>
2015-12-02 10:31:57 +0100

    formatting file

--- fileindex.py
+++ fileindex.py
@@ -3,15 +3,16 @@ import filemeta
 import json
 from server.server import walkFiles
 
-def getIndex(index,dir="./"):
+
+def getIndex(index, dir="./"):
     meta_data_dict = dict()
-    listFiles=[]
-    listFolders=[]
-    walkFiles(listFiles,listFolders,dir)
+    listFiles = []
+    listFolders = []
+    walkFiles(listFiles, listFolders, dir)
     for filename in listFiles:
-        meta_data_dict.setdefault(filename,filemeta.filemeta(filename))
-    index={'listfiles': meta_data_dict, 'listfolders': listFolders}
+        meta_data_dict.setdefault(filename, filemeta.filemeta(filename))
+    index = {'listfiles': meta_data_dict, 'listfolders': listFolders}
     print(index)
 
-# index=[]
-# getIndex(index,dir="./server/store/")
+    # index=[]
+    # getIndex(index,dir="./server/store/")
