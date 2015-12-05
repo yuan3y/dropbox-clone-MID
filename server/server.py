8c3151f7c325eab68218a716356c485bef26edef
Yuan Yiyang <yuan3y@gmail.com>
2015-12-05 16:23:50 +0100

    fix history

--- server/server.py
+++ server/server.py
@@ -9,6 +9,25 @@ app = Flask(__name__)
 
 list_of_client = []
 
+op_history = dict()
+
+def record_history(client=None, operation='remarks', filename='', other=None):
+    # if client not in op_history:
+    #     op_history.setdefault(client, [])
+    #     return
+    global op_history
+    if operation=='remarks':
+        op_history[client].append(list((operation,filename,other)))
+        print(op_history, "hello")
+        return op_history
+    for cl in list_of_client:
+        if cl not in op_history and cl!=client:
+            op_history.setdefault(cl, [])
+        op_history[cl].append(list((operation, filename, other)))
+    # op_history[client].append(operation, filename, other)
+    print(op_history, "hello")
+    return op_history
+
 
 @app.route('/getfile', methods=['GET'])
 # get 'filename' file on the server
@@ -68,56 +87,37 @@ def getIndex():
     return json_result
 
 
-op_history = dict()
 
-
-# class Constant:
-#     file = 1
-#     folder = 2
-#     unknown = 0
-
-
-@app.route('/getHistory', methods=['GET', 'POST'])
+@app.route('/getHistory', methods=['GET'])
 # get a dictionary corresponding clients to a list of operations since the client's last connection
 def getHistory():
+    global op_history
     if request.remote_addr not in list_of_client:
         list_of_client.append(request.remote_addr)
         print('new connection comes from', request.remote_addr)
         op_history.setdefault(request.remote_addr, [])  # start an empty list for new client
-        op_history[request.remote_addr].append(
-            ('remarks', request.remote_addr, datetime.datetime.now(datetime.timezone.utc)))
-    json_result = jsonify({'client': request.remote_addr, 'history': op_history[request.remote_addr].copy()})
+        record_history(request.remote_addr,operation='remarks',filename=request.remote_addr, other=datetime.datetime.now(datetime.timezone.utc))
+    copy = op_history[request.remote_addr].copy()
+    json_result = jsonify({'client': request.remote_addr, 'history': copy})
     op_history[request.remote_addr].clear()
-    op_history[request.remote_addr].append(
-        ('remarks', request.remote_addr, datetime.datetime.now(datetime.timezone.utc)))
+    record_history(request.remote_addr,operation='remarks',filename=request.remote_addr, other=datetime.datetime.now(datetime.timezone.utc))
     return json_result
-
-
-def record_history(client=None, operation='remarks', filename='', other=None):
-    # if client not in op_history:
-    #     op_history.setdefault(client, [])
-    #     return
-    for cl in list_of_client:
-        if cl not in op_history:
-            op_history.setdefault(cl, [])
-        op_history[cl].append(operation, filename, other)
-    # op_history[client].append(operation, filename, other)
-    return
 
 
 @app.route('/del', methods=['POST'])
 # del files/folders on server
 def postDel():
-    if (os.path.isfile(request.form['dir'])):
+    record_history(client=request.remote_addr, operation=request.form['modification'], filename=request.form['dir'],
+                   other=None)
+    if os.path.isfile(request.form['dir']):
         os.remove(request.form['dir'])
     else:
         shutil.rmtree(request.form['dir'])
     # tmp_dict_of_filename_with_client = dict.fromkeys(list_of_client, False)
     # tmp_dict_of_filename_with_client[request.remote_addr] = True
-    record_history(client=request.remote_addr, operation=request.form['modification'], filename=request.form['dir'],
-                   other=None)
     # deleted_files.setdefault(request.form['dir'], tmp_dict_of_filename_with_client)
     # clear_redundant_deleted_files(request.path)
+    return 200
 
 
 @app.route('/change', methods=['POST'])
